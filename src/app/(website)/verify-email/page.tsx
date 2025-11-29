58d4f4c26234343b506730f6f1406599feb4bbbf
Esan Samuel <esansamuel555@gmail.com>
2025-11-29 19:57:19 +0100

    added suspense boundary to useSearchParams

--- src/app/(website)/verify-email/page.tsx
+++ src/app/(website)/verify-email/page.tsx
@@ -1,12 +1,23 @@
 "use client";
+
+import { Suspense } from "react";
 import VerifyEmail from "@/features/website/components/auth/verify-email";
 import { useSearchParams } from "next/navigation";
 
-const Verificationpage = () => {
+const VerificationPage = () => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <InnerVerification />
+    </Suspense>
+  );
+};
+
+const InnerVerification = () => {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
   const username = searchParams.get("fullName");
+
   return <VerifyEmail email={email!} username={username!} />;
 };
 
-export default Verificationpage;
+export default VerificationPage;
