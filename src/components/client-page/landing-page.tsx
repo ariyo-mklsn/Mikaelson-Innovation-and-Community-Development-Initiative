25d8d0d121d2e2eddbd2622be6004210ae570ec3
Olamilekan Aremu <aremu0235@gmail.com>
2026-03-25 20:11:58 +0100

    refactor: eco-system

--- src/components/client-page/landing-page.tsx
+++ src/components/client-page/landing-page.tsx
@@ -1,11 +1,11 @@
 import HomeHero from "@/components/client-page/hero";
 import ImpactSection from "@/features/website/components/impact-section";
-import IntroVideo from "@/components/client-page/intro-video";
+// import IntroVideo from "@/components/client-page/intro-video";
 import BlogSectionWrapper from "./blog-wrapper";
 import WhatWeDo from "./what-we-do";
 import OurEcosystemTabs from "@/components/client-page/our-ecosystem";
 import WhoItsFor from "./audience";
-import CommunitySection from "@/components/client-page/community-section";
+// import CommunitySection from "@/components/client-page/community-section";
 import FinalSection from "@/components/client-page/final-section";
 
 export default function Home() {
